e06834e62da586e030b30fd13b77938ba33745fc
Marko Vujcic <m.vujcic@levi9.com>
2019-08-23 16:14:24 +0200

    Resolvers. Implementing one-to-many (USER --1,M---> Photo) and one-to-one (Photo --1.1--> User) connetions in resolvers

--- index.js
+++ index.js
@@ -3,7 +3,37 @@ const  schemaString      = require('./PhotoSharingSchema').typeDefs;
 const { GraphQLDateTime } = require('graphql-custom-types'); 
 
 let photoId_ = 0;
-let photos = [];
+
+let users = [                                                       // sample users 
+ { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+ { "githubLogin": "gPlake", "name": "Glen Plake" },
+ { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }             
+]
+
+var photos = [                                                      // sample photos
+
+   {
+     "id": "1",
+     "name": "Two Towers",
+     "description": "Union Ortank and Barad Dur towers",
+     "category": "ACTION",
+     "githubUser": "gPlake"                                         // In a RDS table this attribute would be FOREIGN key 
+                                                                    // that references githublogin attr. as prim.key in User table (I think)
+   },
+   {
+     "id": "2",
+     "name": "Enjoying the sunshine",
+     "category": "SELFIE",
+     "githubUser": "sSchmidt"
+   },
+   {
+     "id": "3",
+     "name": "Gunbarrel 25",
+     "description": "25 laps on gunbarrel today",
+     "category": "LANDSCAPE",
+     "githubUser": "sSchmidt"
+   }
+]
 
 const resolvers = {
 
@@ -32,11 +62,25 @@ const resolvers = {
 
    },
   
-   Photo : {                          // Added the so colled Trivial Resolver function that gets each photo object as parent argument
+   Photo : {                          // Added the so called Trivial Resolver function that gets each photo object as parent argument
 
-         url: (parent) => {
-            return `http://yoursite.com/img/${parent.id}.jpg`
-         } 
+      url: (parent) => {
+          return `http://yoursite.com/img/${parent.id}.jpg`
+      },
+      postedBy: (parent) => {
+         return users.find( (u) => { 
+            return parent.githubUser === u.githubLogin
+         }) //  returns single user with githublogin as photo.githubUser
+      }
+   },
+   User: {
+      postedPhotos: (parent) => {
+
+           return photos.filter((p) => { 
+               return p.githubUser === parent.githubLogin           // returns array of photos where githubUser = githublogin 
+            });
+      }
+
    },
 
    DateTime: GraphQLDateTime         // resolver for custom type DateTime
